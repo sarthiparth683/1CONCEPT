b0c27957a6c9e30da9f08ff0d5b9d0e8782cdf28
sarthiparth683 <sarthiparth683@gmail.com>
2024-03-07 23:02:09 +0530

    js

---
+++ Part2JS/1Concept/array/Nodejs.js
@@ -0,0 +1,10 @@
+// 1. cd folderName or fileName (  to reach that folder but naming should be without comma, simple Naming) 
+
+// node -v (to check node is installed or not)
+
+// 2. node fileName.js (to open file)
+// up arrow to reopen or copy previous command 
+
+
+
+console.log("test");
---
+++ Part2JS/1Concept/array/arraylength.js
@@ -0,0 +1,7 @@
+// Create an array with three elements
+const fruits = ["apple", "banana", "orange"];
+
+// Get the length of the array
+const numberOfFruits = fruits.length;
+
+console.log("The array has", numberOfFruits, "fruits.");
