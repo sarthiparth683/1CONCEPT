35a87ffa312ff73e54936bdcd5178ad22cbc0ca6
sarthiparth683 <sarthiparth683@gmail.com>
2024-03-10 23:03:05 +0530

    js

--- Part2JavaScript/1Concept/1Basics/Basics.js
+++ Part2JavaScript/1Concept/1Basics/Basics.js
@@ -1,16 +1,10 @@
-// Checking Data Types
+  // Checking Data Types
 // let x = 12;
 // console.log(typeof(x));
 
 // let x; (variable declaration)
 // x = 11;  (variable assigned)
 // console.log(x);  (variable Printed)
-
-operator
-// / division gives quotient
-// % Modulous gives Remainder
-
-// ** Exponientiation 
 
 // concatination:- 
 // console.log(variable x + variable y );
