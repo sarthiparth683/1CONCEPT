cbe7cdd8ef2ba2f75ba73edc19966d01291a0df0
sarthiparth683 <sarthiparth683@gmail.com>
2024-03-12 23:14:45 +0530

    for loop

---
+++ Part2JavaScript/1Concept/2WhileLoop/while.js
@@ -0,0 +1,7 @@
+ // While Loop
+ let i = 1; // initialize
+ while ( i<=7 ) {    // Condition
+     console.log("Hello World");   // code
+     i = i+1;    // increament
+    // i = i++;   //  i++ give infinite loop in while loop but not in for loop
+ }
--- Part2JavaScript/Day2/test.js
+++ Part2JavaScript/Day2/test.js
@@ -1,5 +1,38 @@
 
-let x = (1 % 2);
-    let y = (1 % 3);
-    // console.log(x);
-    console.log(y);
+
+// let y = (8 % 3);
+// console.log(x);
+
+// let sum = 0;
+// let n = 100;
+// for (let i = 1; i <= n; i+=2) {
+//     sum = sum + i;
+    
+// }
+
+
+// let sum = 0;
+// let n = 100;
+// for (let i = 1; i <= n; i++) {
+//    if (i % 2 === 1){
+//     sum = sum + i;
+//    }
+    
+// }
+// console.log(sum);
+
+
+
+
+
+
+// let mult = 0;
+
+// for (let i = 2; i <= 20; i+=2) {
+//     // mult = mult + i;
+//     console.log(i);
+// }
+    
+
+
+
