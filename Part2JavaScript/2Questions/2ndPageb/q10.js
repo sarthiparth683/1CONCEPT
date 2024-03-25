743d7add381b1b858d327b292c8ab4a0d02dd409
sarthiparth683 <sarthiparth683@gmail.com>
2024-03-25 22:17:24 +0530

    flex-grid-and-jsRevision

--- Part2JavaScript/2Questions/2ndPageb/q10.js
+++ Part2JavaScript/2Questions/2ndPageb/q10.js
@@ -1,16 +1,16 @@
 // 11578-07  Pattern of N
 function patternOfN(N) {
     let count = 0;            // for counting number 
-for (let i = 1; i <=N; i++) {
-    let bag = "";
-    for (let j = 1; j <=N; j++) { // this runs 4 times in each line
-       count = count + 1;           // adding +1 in count
-       bag = bag + count + " ";
+    for (let i = 1; i <= N; i++) {
+        let bag = "";
+        for (let j = 1; j <= N; j++) { // this runs 4 times in each line
+            count = count + 1;           // adding +1 in count
+            bag = bag + count + " ";
+        }
+        console.log(bag);
     }
-    console.log(bag);
-}
 
-   
-} 
+
+}
 
 // patternOfN(5);
