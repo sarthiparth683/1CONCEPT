fe199cdaa7c309d08b2f166452cd152ec776c9b8
sarthiparth683 <sarthiparth683@gmail.com>
2024-04-02 11:47:05 +0530

    updated

---
+++ Part2JavaScript/1Concepts/7Objects/object.js
@@ -0,0 +1,24 @@
+let student = {
+    name:"Rahul",
+    age: 25,
+    gender:"male",
+    city:"Bangalore",
+    hobbies:"coding"
+};
+// console.log(student);
+
+// Accesing object data:-
+// METHOD 1:-
+// console.log(student["name"]);
+// console.log(student["age"]);
+// console.log(student["gender"]);
+
+// METHOD 2:- dot notation
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.hobbies);
+
+// Updating Object:-
+student["age"] = 27;
+student.city = "Hyderabad";
+console.log(student); 
