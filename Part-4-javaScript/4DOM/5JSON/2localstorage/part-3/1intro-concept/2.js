defe3398257d79123ad68f0751003848881d0564
sarthiparth683 <sarthiparth683@gmail.com>
2024-05-09 13:19:27 +0530

    json

---
+++ Part-4-javaScript/4DOM/5JSON/2localstorage/part-3/1intro-concept/2.js
@@ -0,0 +1,16 @@
+// Student Activities
+// Setting and Retrieving Data:
+
+// Task: Write a JavaScript function that allows users to input their name in a text field and store it in local storage when a "Save" button is clicked. Display a message confirming the name has been saved.
+
+// Instructions: Students should create an HTML file with a text input field and a button. They should then write JavaScript code to retrieve the input value when the button is clicked, store it in local storage using the localStorage.setItem() method, and display a confirmation message. Managing Preferences:
+
+// Task: Create a simple web page with two buttons: "Enable Dark Mode" and "Disable Dark Mode". Implement functionality using local storage to toggle between dark and light themes when these buttons are clicked.
+
+// Instructions: Students should write JavaScript code to toggle between dark and light themes by adding or removing a CSS class to the HTML document's body element. They should use local storage to remember the user's preference between page reloads. Shopping Cart Functionality:
+
+// Task: Design a web page with a list of products. Add a "Add to Cart" button for each product. Implement functionality to store the selected products in the user's cart using local storage.
+
+// Instructions: Students should create a product list with multiple items, each having an "Add to Cart" button. When a user clicks the button, the selected product should be stored in local storage as a part of the user's shopping cart. Students should also display the number of items in the cart on the page.
+
+// In conclusion, local storage in web development provides a convenient mechanism for persistently storing data within the user's browser. By leveraging the localStorage API, developers can create seamless user experiences and enhance the functionality of their web applications.
---
+++ Part-4-javaScript/4DOM/5JSON/2localstorage/part-3/problem-1/3.js
@@ -0,0 +1,18 @@
+var ele =    {
+    imageUrl: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/3/5/l/-original-imaghx9qmgqsk9s4.jpeg?q=70',
+    name: 'APPLE iPhone 14 (Blue, 128 GB)',
+    price: '₹57,999'
+  }
+
+var container = document.getElementById("container");
+      var card = document.createElement("div");
+
+      var image = document.createElement("img");
+      image.src = ele.imageUrl
+      var h4 = document.createElement("h4");
+      h4.innerText = ele.name
+      var price = document.createElement("p");
+      price.innerText = ele.price;
+
+    card.append(image,h4,price);
+    container.append(card);
