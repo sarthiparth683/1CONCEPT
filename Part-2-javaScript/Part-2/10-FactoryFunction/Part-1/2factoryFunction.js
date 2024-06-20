0158a9fc6550c03a9102834d2c8ca4817c60417a
sarthiparth683 <sarthiparth683@gmail.com>
2024-06-20 14:40:01 +0530

    1factory

--- Part-2-javaScript/Part-2/10-FactoryFunction/Part-1/2factoryFunction.js
+++ Part-2-javaScript/Part-2/10-FactoryFunction/Part-1/2factoryFunction.js
@@ -127,35 +127,24 @@ console.log(user2);
 
 //-------------------------------------------------------------------------
 // Factory Function Example 2
-
 // create a factory function Student that copy properties and methods from the User factory function and adds an additional property batchno.
 function User(name, age) {
   let userObj = {};
   userObj.name = name;
   userObj.age = age;
-
   userObj.printHello = function() {
       console.log("Hello...");
   };
-
   return userObj;
 }
-
 function Student(name, age, batchno) {
   let studentObj = User(name, age); // get properties and methods from User
-
   studentObj.batchno = batchno; // Add additional property batchno
-
   return studentObj;
 }
-
 let student1 = Student("Pavan", 26, "BatchA");
 let student2 = Student("Roshan", 30, "BatchB");
-
 console.log(student1);
 student1.printHello(); //  method from User
 console.log(student2);
-
-
-
 //-------------------------------------------------------------------------
