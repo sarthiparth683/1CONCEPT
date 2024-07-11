d173a11e7a0b31564f168dd44049aa84ce1cf271
sarthiparth683 <sarthiparth683@gmail.com>
2024-07-11 23:39:05 +0530

    react

--- Part-2-javaScript/Part-1/2Loop/4WhileLoop/while.js
+++ Part-2-javaScript/Part-1/2Loop/4WhileLoop/while.js
@@ -1,13 +1,27 @@
+
+//-----------------------------------------
 // While Loop concept
 let sum = 0;
 let i = 1;
-let n = 5;
+let n = 3;
 while (i <= n) {
     sum = sum + i;
     i++;
 }
-console.log(sum);
-
+// console.log(sum);
+//-------------------------------------------
+let count = 1;
+while (count <= 5) {
+    console.log(count);
+    count++;
+    // console.log(count); 
+};
+//----------------------------------------
+for (let index = 5; index >=0; index--) {
+    // console.log(index)
+    
+}
+//------------------------------------------
 
 
 
