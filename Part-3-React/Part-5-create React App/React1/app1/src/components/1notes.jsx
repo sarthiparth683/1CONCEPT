b96148b131f9d37f9f6d62d13e68f3d8b40abb18
sarthiparth683 <sarthiparth683@gmail.com>
2024-07-19 17:28:07 +0530

    .

--- Part-3-React/Part-5-create React App/React1/app1/src/components/1notes.jsx
+++ Part-3-React/Part-5-create React App/React1/app1/src/components/1notes.jsx
@@ -13,13 +13,14 @@ const Notes = () => {
         </li>
         <li>node -v -to check for latest version of node</li>
         <li>npm install -g npm - might be needed this command</li>
+        <li>npx create-react-app . - dot is for same directory or folder</li>
         <li>npx create-react-app (appName)</li>
         <li>** BEFORE STARTING CHECK RIGHT DIRECTORY</li>
         <li>cd (subdirectory_name) - to go to that particular directory.</li>
         <li>cd .. to move back</li>
         <li>FINAL- npm start - Starts the development server.</li>
       </ol>
-
+ 
       <ol>
         <li>
           important extention for React - ES7+ React/Redux/React-Native snippets{" "}
@@ -55,13 +56,26 @@ const Notes = () => {
         <li></li>
       </ol>
       <ol>
+        <li>3-situations when your component is render</li>
+        <li>when parent is rendering</li>
+        <li>when props are changing</li>
+        <li>when states are changing</li>
+        <li></li>
+      </ol>
+      <ol>
         <li>useEffect Type</li>
         <li>without dependecies[] </li>
+        <li>In mounting</li>
+        <li>state changes/components render</li>
+        {/* <li>useEffect(()=>{})</li> */}
+        <li></li>
         <li>with dependecies[] </li>
-        <li>with [state]</li>
+        <i>in mounting</i>
+        {/* <li>useEffect(()=>{},[state])</li> */}
         <li></li>
+        {/* <li>useEffect(()=>{opearationretutn ()=>{}},[]) </li> */}
         <li></li>
-        <li></li>
+        {/* <li>==> cleanup function of useEffect.</li> */}
         <li></li>
       </ol>
     </div>
--- Part-3-React/Part-5-create React App/React2/app2/src/App.js
+++ Part-3-React/Part-5-create React App/React2/app2/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 222222222222
-        </a>
-      </header>
+      <Product/>
+      <h1>11111111</h1>
     </div>
   );
 }
 
 export default App;
+ 
