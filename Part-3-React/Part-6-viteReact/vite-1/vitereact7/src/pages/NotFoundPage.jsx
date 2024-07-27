54c8c41c69b8243a9d3234e6967531768539e7eb
sarthiparth683 <sarthiparth683@gmail.com>
2024-07-27 23:06:19 +0530

    .

---
+++ Part-3-React/Part-6-viteReact/vite-1/vitereact7/src/pages/NotFoundPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src="https://th.bing.com/th/id/OIP.XGashdGx2lVmeHmbgSKczwHaD7?w=312&h=180&c=7&r=0&o=5&pid=1.7"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default NotFoundPage;
