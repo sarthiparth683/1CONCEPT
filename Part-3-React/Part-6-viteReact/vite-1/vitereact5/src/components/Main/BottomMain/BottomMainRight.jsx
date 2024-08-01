ef8ff0bb3f868c1de228ad33ed1790b2a260ae57
sarthiparth683 <sarthiparth683@gmail.com>
2024-08-01 17:21:40 +0530

    .

--- Part-3-React/Part-6-viteReact/vite-1/vitereact5/src/components/Main/BottomMain/BottomMainRight.jsx
+++ Part-3-React/Part-6-viteReact/vite-1/vitereact5/src/components/Main/BottomMain/BottomMainRight.jsx
@@ -6,7 +6,9 @@ const BottomMainRight = () => {
   const { isloggedin, login, logout, test } = useContext(AuthContext);
   console.log(test)
   return (
+
     <div className="bottommainright">
+        <h1> {test}  </h1>
       <p> BottomMainRight: {isloggedin ? "LoggedIn" : "Not LoggedIn"}</p>
       <button onClick={login}>login</button>
       <button onClick={logout}>logout</button>
