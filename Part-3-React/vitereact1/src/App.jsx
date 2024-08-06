e9cec44205da3fd07194a64b814b8d51e3ca71f2
sarthiparth683 <sarthiparth683@gmail.com>
2024-08-06 23:11:21 +0530

    .

--- Part-3-React/vitereact1/src/App.jsx
+++ Part-3-React/vitereact1/src/App.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Comp11 from "./component11/Comp11";
+import Comp12 from "./component12/comp12";
 
 const App = () => {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Vite + React Components:-</h1>
-      {/* <  Comp1/> */}
-      {/* < Comp2 /> */}
+      {/* <Comp1/> */}
+      {/* <Comp2 /> */}
       {/* <Comp3 /> */}
       {/* <Comp4 /> */}
-      {/* < Comp8 /> */}
+      {/* <Comp8 /> */}
       {/* <Comp9/> */}
       {/* <Comp10 /> */}
-      <Comp11 />
+      {/* <Comp11 /> */}
+      <Comp12 />
       {/* <Counter /> */}
       {/* < Timer /> */}
     </>
---
+++ Part-3-React/vitereact1/src/component12/comp12.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Comp12 = () => {
+  return <div>Comp12</div>;
+};
+
+export default Comp12;
