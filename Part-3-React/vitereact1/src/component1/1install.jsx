6db5ddd19eeb8262f09f5f67bae8abb7d20517bd
sarthiparth683 <sarthiparth683@gmail.com>
2024-08-10 05:10:29 +0530

    install

--- Part-3-React/vitereact1/src/component1/1install.jsx
+++ Part-3-React/vitereact1/src/component1/1install.jsx
@@ -5,6 +5,25 @@ const Notes = () => {
     <div>
       <ol>
         <li>
+          <h1>Vite-installation</h1>
+        </li>
+        <li>node - v -check node version if not then install node</li>
+        <li>npm create vite@latest . -- --template react -</li>
+        <li>cd folder name or cd .. to go back</li>
+        <li>npm install- for installing node modules,Dependencies.</li>
+        <li>npm run dev - to start the app</li>
+      </ol>
+
+      <ol>
+        <li>Hoisting on other devices in vite</li>
+        <li>
+          in packaage.json file - need to change -"scripts": "dev": "vite
+          --host",
+        </li>
+      </ol>
+
+      <ol>
+        <li>
           <h1>React App installation:-</h1>
         </li>
         <li>1.Node install latest version - then check version - node -v</li>
@@ -20,30 +39,6 @@ const Notes = () => {
         <li>cd (subdirectory_name) - to go to that particular directory.</li>
         <li>cd .. to move back</li>
         <li>4.FINAL- npm start - Starts the development server.</li>
-      </ol>
-
-      <ol>
-        <li>
-          <h1>vite installation</h1>
-        </li>
-        <li>
-          Install Node.js: Ensure you have Node.js current node version - node -
-          v;
-        </li>
-        <li>npm create vite@latest . -- --template react -</li>
-        <li>cd folder name or cd .. to go back</li>
-        <li>npm install- for installing node modules,Dependencies.</li>
-        <li>npm run dev - to start the app</li>
-      </ol>
-
-      <ol>
-        <li>
-          <b>Hoisting on other devices in vite</b>
-        </li>
-        <li>
-          in packaage.json file - need to change - "dev": "vite", into
-          -------this - "scripts": "dev": "vite --host",
-        </li>
       </ol>
     </div>
   );
