cf2e5a8cacda8e1803dc51fc0bf5bcc27f228849
Parth Sarthi <132654950+Parthsarthi-ui@users.noreply.github.com>
2024-08-29 06:59:14 +0530

    .

--- Part-3-React/vitereact1/src/component1/1install.jsx
+++ Part-3-React/vitereact1/src/component1/1install.jsx
@@ -6,9 +6,9 @@ const Notes = () => {
       <ol>
         <li>installing npm in React</li>
         <li>install node</li>
-        <li>node - v -check node version</li>
+        <li>node - v -check node version</li>   
         <li>npm create vite@latest . -- --template react -</li>
-        <li>cd folder name or cd .. to go back</li>
+        <li>cd folder name or cd .. to go back</li>  
         <li>npm install- for installing node modules,Dependencies.</li>
         <li>npm run dev - to start the app</li>
       </ol>
