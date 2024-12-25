66e3bef18d1fe01ef195f197cb7201c487f6b810
Parth Sarthi <sartiparth683@.com>
2024-12-25 17:07:51 +0530

    express

--- Part-5-Expressjs/q3/readme.md
+++ Part-5-Expressjs/q3/readme.md
@@ -5,9 +5,16 @@ Express.js is external module so it need to be installed so
     now, ready to use express.js
 
                 NODEMON
+
 3.  npm i nodemon - install nodemon - to prevent the re running the server
 4.  To run file after installing nodemon
 5.  in package.json file - in "scripts"
 6.  create - "server": "nodemon index.js"
 7.  npm run server - in terminal
- 
+
+                STATIC FILES - setup is done to add external css and js files in public folder
+
+8.  create public folder
+9.  create 3 folders inside it - images, stylesheets, javascripts
+10. configure the express static in script.js file
+11. understand the path
--- Part-5-Expressjs/q3/server1.js
+++ Part-5-Expressjs/q3/server1.js
@@ -1,9 +1,9 @@
-// ejs in express
-// tempelate engines example - ejs
+//   STATIC FILES
 const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("./public")); //   STATIC FILES
 
 app.get("/", function (req, res) {
   res.render("index", { age: 12 });
@@ -19,6 +19,23 @@ app.get("/contact", function (req, res) {
 
 app.get("/snow", function (req, res) {
   res.render("snow");
+});
+
+// error handeling in express
+app.get("/error1", function (req, res, next) {
+  throw Error("Something Went Wrong!");
+});
+
+app.get("/error2", function (req, res) {
+  throw Error(" i don't know Something Went Wrong!.......");
+});
+
+app.use(function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render("error", { error: err });
 });
 
 app.listen(3000, () => {
