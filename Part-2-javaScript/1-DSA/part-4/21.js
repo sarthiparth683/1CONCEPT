5106df970bd94f5b0ba15e6aad66892fb1bec6c4
Parth Sarthi <sartiparth683@.com>
2025-02-06 00:54:26 +0530

    .

---
+++ Part-2-javaScript/1-DSA/part-4/21.js
@@ -0,0 +1,16 @@
+// let num = 7;
+// let prime = true;
+// for (let i = 2; i < num; i++) {
+//   if (num % i === 0) {
+//     prime = false;
+//   }
+// }
+// console.log(prime);
+//---------------------------------------------------------
+
+for (let i = 1; i <= 10; i++) {
+  if (i == 5) {
+    console.log(i);
+    continue;
+  }
+}
