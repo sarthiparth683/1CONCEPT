ecb7fffaa77dc5dd9fcfdd79c28721c39cca8608
Parth Sarthi <sartiparth683@.com>
2025-07-08 02:36:37 +0530

    dsa-linkedLIst

--- Part-6-DSA/1Basics/3LInkidlist/1.js
+++ Part-6-DSA/1Basics/3LInkidlist/1.js
@@ -1,69 +1,47 @@
 //-----------------------------------------------------
-// Set
-
-// function removeDuplicates(arr) {
-//   let set = [...new Set(arr)];
-//   return set;
-// }
-// console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
-//-----------------------------------------------------
-// function allUnique(arr) {
-//   return new Set(arr).size === arr.length;
-// }
-
-// console.log(allUnique([1, 2, 3])); // true
-// console.log(allUnique([1, 2, 2])); // false
-//-----------------------------------------------------
-// function intersection(arr1, arr2) {
-//   let set1 = new Set(arr1);
-//   return arr2.filter((el) => set1.has(el));
-// }
-// console.log(intersection([1, 2, 3], [2, 3, 4])); // [2, 3]
-// console.log(intersection([5, 6, 7], [7, 8, 9])); // [7]
-//-----------------------------------------------------
-// let mySet = new Set(["apple", "banana", "cherry"]);
-// mySet.delete("banana")
-// console.log(mySet);
-//-----------------------------------------------------
-// function uniqueCharCount(str) {
-//  let set = new Set(str)
-//  return set.size
-// }
-
-// console.log(uniqueCharCount("hello"));
-// console.log(uniqueCharCount("banana"));
-//-----------------------------------------------------
-//  removes all vowels (a, e, i, o, u) from a string
-
-// function removeVowels(str) {
-//   let set = new Set("aeiou");
-//   return str.split("").filter((el) => !set.has(el)).join("");
-// }
-// console.log(removeVowels("hello"));
-// console.log(removeVowels("beautiful"));
-//-----------------------------------------------------
-// Find duplicates in an array
-
-// function findDuplicates(arr) {
-//   let seen = new Set();
-//   let dupes = new Set();
-
-//   for (let el of arr) {
-//     if (seen.has(el)) {
-//       dupes.add(el);
-//     } else {
-//       seen.add(el);
-//     }
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
 //   }
-//   return [...dupes];
 // }
 
-// console.log(findDuplicates([1, 2, 2, 3, 3, 3, 4])); 
-// console.log(findDuplicates([5, 5, 5, 5, 5])); 
-// console.log(findDuplicates([1, 2, 3])); 
+// let firstNode = new Node(10);
+// let secondNode = new Node(20);
+// firstNode.next = secondNode;
+// console.log(firstNode);
+// console.log(firstNode.next);
 //-----------------------------------------------------
-//-----------------------------------------------------
-//-----------------------------------------------------
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  append(value) {
+    let newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+}
+
+let list = new LinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+console.log(list.head);
 //-----------------------------------------------------
 //-----------------------------------------------------
 //-----------------------------------------------------
