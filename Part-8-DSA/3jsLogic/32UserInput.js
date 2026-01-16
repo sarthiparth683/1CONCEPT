e581289764fd359b336bff619bc08caf32be6b56
Parth Sarthi <sarthiparth683@gmail.com>
2026-01-16 22:50:48 +0530

    dsa

--- Part-8-DSA/3jsLogic/32UserInput.js
+++ Part-8-DSA/3jsLogic/32UserInput.js
@@ -1,4 +1,5 @@
-const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")();  // to run in node not in browser
 // console.log(prompt)
 const nameOfTheUser = prompt("Please tell your name: ");
 console.log(nameOfTheUser);
+ 
---
+++ Part-8-DSA/3jsLogic/33RollADice.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync")(); // to run in node not in browser
+
+function rollADice(sidesInADice) {
+  return Math.floor(Math.random() * sidesInADice) + 1;
+}
+
+function rollMultipleDices(numberOfDice, sidesInADice) {
+  const result = [];
+  let counter = 0;
+  while (counter < numberOfDice) {
+    const rollADiceResult = rollADice(sidesInADice);
+    result.push(rollADiceResult);
+    counter++;
+  }
+
+  return result;
+}
+
+const numberOfDices = prompt("Please tell number of dices: ");
+const numberOfSides = prompt("Please tell number of sides in a dice: ");
+console.log(rollMultipleDices(numberOfDices, numberOfSides));
+// console.log(rollMultipleDices(3, 6));
+// =====================================================================================
