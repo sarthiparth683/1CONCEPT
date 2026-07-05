604cc16784f519a7fab8c427e56ee9e7853429bc
Parth Sarthi <sarthiparth683@gmail.com>
2026-07-05 20:31:30 +0530

    .

--- Part-7-Projects/2ProCode/3jumping-letters/index.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelectorAll("span");
-console.log(container);
-container.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.classList.toggle("active");  //  activate and deactivate "active class" for span tag
-  });
-});
-//-----------------------------------------------------
